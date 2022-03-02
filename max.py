9d2dba84db6e1608da2861b5782861a0c42b3446
salshii <94897334+salshii@users.noreply.github.com>
2022-03-02 15:37:40 +0530

    klklkl

---
+++ max.py
@@ -0,0 +1,9 @@
+from math import*
+n=float(input("enter the number"))
+m=float(input("enter the number"))
+print("Max=",max(n,m))
+print("Min=",min(n,m))
+print("Power",pow(n,m))
+print("modf",modf(n))
+print("sqrt",sqrt(m))
+      
